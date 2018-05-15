6b9b488b526ecffe2e3b3d785f3bc5644d07b035
Abiral Poudel <abiral.p95@gmail.com>
2018-05-15 11:03:44 +0545

    InitialModel migration added

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,23 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //enabling attribute routing
+            routes.MapMvcAttributeRoutes();
+
+            //    routes.MapRoute(
+            //       "MoviesByReleaseDate",
+            //       "movies/Released/{year}/{month}",
+            //       new {controller="Movies", action = "ByReleaseDate"},
+            //     //now putting constraints
+            //    new {year = @"\d{4}", month = @"\d{2}"}
+            //    //regular expressions are used..if you dont use '@' put double slash \\
+            //   //if u want to put year constraints to 2015 and 2016 only then @"2015|2016"
+            //  );
+
+
+
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
